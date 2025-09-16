149cb610f1a9ce8c104c03193de823e2883d6592
filipcobanin <fcobanin@gmail.com>
2025-09-16 12:44:33 +0200

    Initial commit

---
+++ src/main/kotlin/com/jetbrains/youtrapbackend/YouTrapBackendApplication.kt
@@ -0,0 +1,11 @@
+package com.jetbrains.youtrapbackend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class YouTrapBackendApplication
+
+fun main(args: Array<String>) {
+    runApplication<YouTrapBackendApplication>(*args)
+}
---
+++ src/test/kotlin/com/jetbrains/youtrapbackend/YouTrapBackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.jetbrains.youtrapbackend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class YouTrapBackendApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
